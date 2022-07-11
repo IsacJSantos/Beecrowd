83059f744a91a940ef893ea9e3aba2743c05b962
Isac Santos <isac4322@gmai.com>
2022-07-11 17:56:43 -0300

    "Esfera" Solved

---
+++ Iniciante/Esfera/Program.cs
@@ -0,0 +1,14 @@
+﻿using System; 
+
+class URI {
+    const double PI = 3.14159;
+    static void Main(string[] args) { 
+        double r = double.Parse(Console.ReadLine());
+        SphereVolume(r);
+    }
+
+    static void SphereVolume(double radius){
+        double volume = (4.0/3.0) * PI * Math.Pow(radius,3);
+        Console.WriteLine($"VOLUME = {volume.ToString("F3")}");
+    }
+}
