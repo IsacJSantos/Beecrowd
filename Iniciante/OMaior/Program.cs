c21414e65566e664a63cb0c3786c7d3cd9cc1f04
Isac Santos <isac4322@gmai.com>
2022-07-12 17:17:02 -0300

    "O Maior" Solved

---
+++ Iniciante/OMaior/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        string[] s = Console.ReadLine().Split(' ');
+        int a = int.Parse(s[0]);
+        int b = int.Parse(s[1]);
+        int c = int.Parse(s[2]);
+        LargerNumber(a, b, c);
+    }
+    static void LargerNumber(int A, int B, int C)
+    {
+        Console.WriteLine(Math.Max(A, Math.Max(B, C)) + " eh o maior");
+    }
+}
