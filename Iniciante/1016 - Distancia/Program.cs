e144fd7a127e779c35733ee56ae43d3c8830fa96
Isac Santos <isac4322@gmai.com>
2022-07-15 17:19:06 -0300

    "Distancia" Solved

---
+++ Iniciante/1016 - Distancia/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int km = int.Parse(Console.ReadLine());
+        Resolution(km);
+    }
+
+    static void Resolution(int km)
+    {
+        Console.WriteLine($"{km * 2} minutos");
+    }
+
+}
