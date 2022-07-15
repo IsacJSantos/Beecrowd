d0a796d41a86d3660c3ec4ec319f0ec8d681f1a3
Isac Santos <isac4322@gmai.com>
2022-07-15 17:33:20 -0300

    "Gasto de Combustivel" Solved

---
+++ Iniciante/1017 - GastoDeCombustivel/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI
+{
+    const int KML = 12;
+    static void Main(string[] args)
+    {
+        int hours = int.Parse(Console.ReadLine());
+        int avrSpeed = int.Parse(Console.ReadLine());
+        Calc(hours, avrSpeed);
+    }
+
+    static void Calc(int hours, int avrSpeed)
+    {
+        double distance = hours * avrSpeed;
+        Console.WriteLine((distance / KML).ToString("F3"));
+    }
+
+}
